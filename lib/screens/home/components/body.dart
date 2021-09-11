33982578a05755205ab51b7c0c21a93c25d159df
Tanay Kamath <2tanaykamath@gmail.com>
2021-09-11 18:10:51 +0530

    add FeaturedPlants

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:plant_app/constants.dart';
 
+
+import '../../../constants.dart';
+import 'featured_plants.dart';
 import 'header_with_searchbox.dart';
 import 'recommended_plants.dart';
 import 'title_with_more_btn.dart';
@@ -18,62 +20,10 @@ class Body extends StatelessWidget {
           RecommendedPlants(),
           TitleWithMoreBtn(title: "Featured Plants", onMoreBtnPressed: () {}),
           FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
         ],
       ),
     );
   }
 }
 
-class FeaturedPlants extends StatelessWidget {
-  const FeaturedPlants({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        children: [
-          FeaturedPlantCard(image: "assets/images/bottom_img_1.png", press: () {}),
-          FeaturedPlantCard(image: "assets/images/bottom_img_2.png", press: () {}),
-        ],
-      ),
-    );
-  }
-}
-
-class FeaturedPlantCard extends StatelessWidget {
-  const FeaturedPlantCard({
-    Key? key,
-    required this.image,
-    required this.press,
-  }) : super(key: key);
-
-  final String image;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-        margin: EdgeInsets.only(
-          left: kDefaultPadding,
-          top: kDefaultPadding / 2,
-          bottom: kDefaultPadding / 2,
-        ),
-        width: size.width * 0.8,
-        height: 185,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-            fit: BoxFit.cover,
-            image: AssetImage(image),
-          ),
-        ),
-      ),
-    );
-  }
-}
